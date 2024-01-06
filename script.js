941ae34038c754e67fad8bce3f4eebdaf5c533d1
YourNewUsername <snjha2000881@gmail.com>
2024-01-06 16:59:52 +0530

    send-message with appropriate information in function

--- script.js
+++ script.js
@@ -7,10 +7,29 @@ document.addEventListener('DOMContentLoaded', function () {
         userName.textContent=`${name}`;
         userName.style.display='block';
     };
+
     const profileImage = document.querySelector('.profile');
-    const profileSelect=document.querySelector('.img-select');
-    
+    const profileSelect = document.querySelector('.img-select');
+
     profileImage.addEventListener('click', function () {
-    profileSelect.click();
+        profileSelect.click();
     });
+    const client_message = document.querySelector(".send-button");
+
+    function sendMessage() {
+
+        var messageInput = document.getElementById("textInput");
+        var message = messageInput.value;
+
+        alert("Message sent: " + message);
+
+        messageInput.value = "";
+
+    }
+
+    client_message.addEventListener("click", sendMessage);
 });
+
+
+
+
