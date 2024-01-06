3f320f31b6ee607fca771a3fabe8a807df6f0b5f
YourNewUsername <snjha2000881@gmail.com>
2024-01-06 17:39:23 +0530

    bug removed prompt name

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-     let name;
-     let userName=document.querySelector('.profile_name');
-    do {
-        name=prompt('please enter your name:')
-    } while (!name){
-        userName.textContent=`${name}`;
-        userName.style.display='block';
-    };
+    let name;
+    let userName = document.querySelector('.profile_name');
+
+    while (!name) {
+        name = prompt('Please enter your name:');
+    }
+
+    userName.textContent = `${name}`;
+    userName.style.display = 'block';
 
     const profileImage = document.querySelector('.profile');
     const profileSelect = document.querySelector('.img-select');
@@ -14,22 +15,23 @@ document.addEventListener('DOMContentLoaded', function () {
     profileImage.addEventListener('click', function () {
         profileSelect.click();
     });
+
     const client_message = document.querySelector(".send-button");
 
     function sendMessage() {
-
         var messageInput = document.getElementById("textInput");
         var message = messageInput.value;
 
-        alert("Message sent: " + message);
+        alert("Message sent: " + message + " by " + name);
 
         messageInput.value = "";
-
     }
 
     client_message.addEventListener("click", sendMessage);
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    });
 });
-
-
-
-
