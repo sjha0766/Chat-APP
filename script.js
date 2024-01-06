0c60de19f70beba692aa8970d029f7a86c0d9c3e
YourNewUsername <snjha2000881@gmail.com>
2024-01-06 18:31:45 +0530

    message area sender reciver resolved

--- script.js
+++ script.js
@@ -16,19 +16,30 @@ document.addEventListener('DOMContentLoaded', function () {
         profileSelect.click();
     });
 
+
+    function appendMessage(message, sender) {
+        var ul = document.querySelector(".list-group");
+        var li = document.createElement("li");
+    
+        li.className = "list-group-item list-group-item-" + (sender === "sender" ? "primary align-left" : "success align-right");
+        console.log(li.className);
+        li.appendChild(document.createTextNode(message));
+        ul.appendChild(li);
+    }
     const client_message = document.querySelector(".send-button");
 
     function sendMessage() {
         var messageInput = document.getElementById("textInput");
         var message = messageInput.value;
 
-        alert("Message sent: " + message + " by " + name);
+        
+    
+        appendMessage(message,"sender");
 
         messageInput.value = "";
     }
 
     client_message.addEventListener("click", sendMessage);
-
     document.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             sendMessage();
