22aab8ce1237e56e4ab83d1536749aaa4a847ff8
sjha0766 <sjha0766@gmail.com>
2024-01-06 16:58:54 +0530

    add prompt

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
- 
+     let name;
+     let userName=document.querySelector('.profile_name');
+    do {
+        name=prompt('please enter your name:')
+    } while (!name){
+        userName.textContent=`${name}`;
+        userName.style.display='block';
+    };
     const profileImage = document.querySelector('.profile');
     const profileSelect=document.querySelector('.img-select');
     
