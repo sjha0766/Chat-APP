95844ac3d8d311c4239c5a41046866b33b274583
AyushChaudhary2002 <ayush.chaudhary@comprotechnologies.com>
2024-01-08 22:38:27 +0530

    message endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import './config/db.js';
 import { Server as socketIo } from 'socket.io';
 import http from 'node:http';
+import router from './routers/index.js';
 import 'dotenv/config';
 
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const server = http.createServer(app);
 const io = new socketIo(server);
 
 app.use(express.json());
+app.use(router);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
